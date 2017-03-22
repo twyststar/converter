2bb8ab8d96d9fc4a0690ef452c7a697b70b8ee7a
Starlynne <lewis.start0905@gmail.com>
2017-03-22 14:37:44 -0700

    add second temp conversion jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var findCelcius = function(number1) {
   return ((number1 - 32) / 1.8).toFixed(2);
 };
-
+var findFahrenheit = function(number1) {
+  return (number1 * 1.8  + 32).toFixed(2);
+};
 
 $(document).ready(function(){
   $("#FToC").submit(function(event){
@@ -10,6 +12,11 @@ $(document).ready(function(){
     $("#outputFToC").text(result);
     event.preventDefault();
   });
-
+  $("#CToF").submit(function(event){
+    var number1 = parseInt($("#CToF1").val());
+    var result = findFahrenheit(number1);
+    $("#outputCToF").text(result);
+    event.preventDefault();
+  });
 
 });
